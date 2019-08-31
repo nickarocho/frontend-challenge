df56fce4e480804105ef2356f875de76a3561218
Nick Arocho <nick@boldlyfine.com>
2019-08-31 16:44:53 -0700

    Fetch data from endpoint, construct data object.

--- main.js
+++ main.js
@@ -1,16 +1,38 @@
-Vue.component("hero", {
-  template: `
-    <section id="hero" class="hero">
-        <img src="#" alt="" class="hero__background" />
-        <div class="card">
-            <h1 class="card__title">Title</h1>
-            <p class="card__body">Body</p>
-            <button href="#" class="card__btn">Click Me</button>
-        </div>
-    </section>
-    `
-});
+let url = "https://www.dermstore.com/ajax/challenge.php";
+let payload;
 
-var app = new Vue({
-  el: "#app"
+fetch(url).then(function(res) {
+  res
+    .text()
+    .then(function(text) {
+      payload = JSON.parse(text);
+      console.log(payload);
+    })
+    .then(function() {
+      Vue.component("hero", {
+        template: `
+            <section id="hero" class="hero">
+                <img v-bind:src="backgroundImg" alt="" class="hero__background" />
+                <div class="card">
+                    <h1 class="card__title">{{ payload.title }}</h1>
+                    <p class="card__body">Body</p>
+                    <button href="#" class="card__btn">Click Me</button>
+                </div>
+            </section>
+        `,
+        data() {
+          return {
+            backgroundImg: payload.background,
+            cardTitle: payload.title,
+            cardBody: payload.body,
+            cardBtnTxt: payload.button.text,
+            cardBtnHref: payload.button.href
+          };
+        }
+      });
+
+      var app = new Vue({
+        el: "#app"
+      });
+    });
 });

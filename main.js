fe43412eca81accecb82c93c11552356a7732492
Nick Arocho <nick@boldlyfine.com>
2019-08-31 16:10:24 -0700

    Convert to Vue app, add basic hero template structure.

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-alert("up and running!");
+Vue.component("hero", {
+  template: `
+    <section id="hero" class="hero">
+        <img src="#" alt="" class="hero__background" />
+        <div class="card">
+            <h1 class="card__title">Title</h1>
+            <p class="card__body">Body</p>
+            <button href="#" class="card__btn">Click Me</button>
+        </div>
+    </section>
+    `
+});
+
+var app = new Vue({
+  el: "#app"
+});
